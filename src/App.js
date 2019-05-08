977a562ca7eee945629773a97f46b87534251197
dugi <contact@tusk.world>
2019-05-08 03:42:59 +0200

    all files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+// import Box from "./Box";
+import BoxContainer from "./BoxContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Something</h1>
+      <BoxContainer />
+    </div>
+  );
+}
+
+export default App;
